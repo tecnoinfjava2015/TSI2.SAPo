480c1b3690475035c3171bfd8f2d1e3fec591cd7
jpmartinez <jpmc21891@gmail.com>
2015-09-27 18:07:00 -0300

    Arreglo en el test.

--- SAPo-dal/test/com/dal/test/mongo/CategoryTest.java
+++ SAPo-dal/test/com/dal/test/mongo/CategoryTest.java
@@ -6,6 +6,7 @@ import org.bson.types.ObjectId;
 import org.junit.Test;
 
 import com.entities.mongo.Category;
+import com.entities.mongo.dao.CategoryDAO;
 import com.mongo.utilities.GenericPersistence;
 import com.mongo.utilities.MongoDB;
 
@@ -13,10 +14,13 @@ public class CategoryTest {
 
 	 protected MongoDB persistence;
 	  protected GenericPersistence genericPersistence = new GenericPersistence();
+	  protected CategoryDAO catDAO = new CategoryDAO();
 	  @Test
 	  public void persist() {
+		catDAO.cleanCategories("T1");
 	    Category category = new Category();
 	    category.setName("Test category");
+	    category.setTenant("T1");
 	    ObjectId id1 = genericPersistence.persist(category);
 	    assertNotNull("Debería haberse generado un ObjectId cuando se persistio la entidad", id1);
 	    assertEquals("El valor existente en la base y el generado deberían ser el mismo",
@@ -28,11 +32,13 @@ public class CategoryTest {
 	   */
 	  @Test
 	  public void count() {
+		  catDAO.cleanCategories("T1");
 	    assertEquals("En una base vacía deberíamos tener 0 categorías.", 0,
 			 genericPersistence.count(Category.class));
 	    
 	    Category category = new Category();
 	    category.setName("Test category");
+	    category.setTenant("T1");
 	    genericPersistence.persist(category);
 	 
 	    assertEquals("Después de haber agregado una categoría debería haber 1", 1,
