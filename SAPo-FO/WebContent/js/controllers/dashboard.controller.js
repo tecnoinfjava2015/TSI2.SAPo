4f6a13bdb7598b60d85b0d6197948859c28cf5de
Lucia Salvarrey <lusalvarrey@gmai.com>
2015-11-08 18:32:52 -0300

    added logic to get tenant name from the url

--- SAPo-FO/WebContent/js/controllers/dashboard.controller.js
+++ SAPo-FO/WebContent/js/controllers/dashboard.controller.js
@@ -3,9 +3,19 @@
     angular
         .module('sapo')
         .controller('DashboardController', DashboardController);
-    DashboardController.$inject = ['$scope'];
+    DashboardController.$inject = ['$scope', '$routeParams'];
     /* @ngInject */
-    function DashboardController($scope) {
-    	$scope.title = 'Dashboard';    	
+    function DashboardController($scope, $routeParams) {
+    	$scope.title = 'Dashboard';   
+    	
+    	var tenant = $routeParams.tenantName;
+    	
+    	if (typeof tenant !== 'undefined') {
+    		$scope.disableReport = false;
+    	}
+    	else {
+    		$scope.disableReport = true;
+    	}
+    	console.log(tenant);
     }
 })();
