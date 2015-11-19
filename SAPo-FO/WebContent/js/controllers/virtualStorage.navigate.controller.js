fc52f3e5499eac2e9c966ea03ea76f425ae24e4c
Lucia Salvarrey <lusalvarrey@gmai.com>
2015-11-18 21:07:59 -0300

    added validation for different account types

--- SAPo-FO/WebContent/js/controllers/virtualStorage.navigate.controller.js
+++ SAPo-FO/WebContent/js/controllers/virtualStorage.navigate.controller.js
@@ -3,15 +3,23 @@
     angular
         .module('sapo')
         .controller('VirtualStorageNavigateController', VirtualStorageNavigateController);
-    VirtualStorageNavigateController.$inject = ['VirtualStorageViewResource', '$scope', '$location', '$mdDialog', '$rootScope'];
+    VirtualStorageNavigateController.$inject = ['VirtualStorageViewResource', '$scope', '$location', '$mdDialog', '$rootScope', '$cookies'];
     /* @ngInject */
-    function VirtualStorageNavigateController(VirtualStorageViewResource, $scope, $location, $mdDialog, $rootScope) {
+    function VirtualStorageNavigateController(VirtualStorageViewResource, $scope, $location, $mdDialog, $rootScope, $cookies) {
+    	var loggedUser = $cookies.getObject("sapoUser");
+    	var virtualStorages = $cookies.getObject("sapoVirtualStorages");
+    	/*
+    	alert(loggedUser.type === 'Free' && virtualStorages.owned.length > 1);
+    	*/
+    	
     	var vm = this;
     	$scope.virtualStorages = {};
     	$scope.title = 'Almacenes virtuales';    
     	
     	$scope.showAdvanced = showAdvanced;
     	$scope.editVirtualStorage = editVirtualStorage;
+    	$scope.cancel = cancel;
+    	$scope.showAlert = showAlert;
     	
     	VirtualStorageViewResource.query({
             oId: 151
@@ -30,6 +38,7 @@
     	function editVirtualStorage(ev, virtualStorage) {
     		$rootScope.tenantName = virtualStorage.name;
     		$rootScope.tenantId = virtualStorage.id;
+    		$rootScope.ownerId = virtualStorage.owner;
     		$mdDialog.show({
     	    	controller: 'VirtualStorageEditController',
                 templateUrl: 'templates/virtualstorage.edit.html',
@@ -46,21 +55,50 @@
     		//$location.url('http://localhost:8080/SAPo-FO/index.html#/virtualStorage/' + virtualStorageName + '/edit');
     	}
     	
+    	function cancel() {
+    		$mdDialog.cancel();
+		};
+		
+		function showAlert(title,content) {
+			// Appending dialog to document.body to cover sidenav in docs app
+			// Modal dialogs should fully cover application
+			// to prevent interaction outside of dialog
+			$mdDialog
+					.show($mdDialog
+							.alert()
+							.parent(
+									angular.element(document
+											.querySelector('#popupContainer')))
+							.clickOutsideToClose(true)
+							.title(title)
+							.content(content)
+							.ariaLabel('Alert Dialog Demo').ok('Cerrar'));
+		};
+    	
     	
     	function showAdvanced(ev) {
-    	    $mdDialog.show({
-    	    	controller: 'VirtualStorageController',
-                templateUrl: 'templates/virtualStorage.create.html',
-    	    	
-    	        parent: angular.element(document.body),
-    	        targetEvent: ev,
-    	        clickOutsideToClose:true
-    	    })
-    	    .then(function(answer) {
-    	        $scope.status = 'You said the information was "' + answer + '".';
-    	    }, function() {
-    	        $scope.status = 'You cancelled the dialog.';
-    	    });
+    		if (loggedUser.type === 'Free' && virtualStorages.owned.length > 0) {
+    			showAlert('Error!', 'Usted ha alcanzado el m&aacute;ximo de almacenes virtuales permitidos.</br>Si desea agregar m&aacute;s almacenes virutales debe realizar un upgrade de su cuenta.');
+    		}
+    		else if (loggedUser.type === 'FREEMIUM' && virtualStorages.owned.length > 1) {
+    			showAlert('Error!', 'Usted ha alcanzado el m&aacute;ximo de almacenes virtuales permitidos.</br>Si desea agregar m&aacute;s almacenes virutales debe realizar un upgrade de su cuenta.');
+    		}
+    		else {
+    			$mdDialog.show({
+        	    	controller: 'VirtualStorageController',
+                    templateUrl: 'templates/virtualStorage.create.html',
+        	    	
+        	        parent: angular.element(document.body),
+        	        targetEvent: ev,
+        	        clickOutsideToClose:true
+        	    })
+        	    .then(function(answer) {
+        	        $scope.status = 'You said the information was "' + answer + '".';
+        	    }, function() {
+        	        $scope.status = 'You cancelled the dialog.';
+        	    });
+    		}
+    	    
     	  };
     }
 })();
