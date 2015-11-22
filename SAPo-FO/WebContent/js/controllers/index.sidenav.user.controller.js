4a5f16bb36c493601b9151cc28da7d7a07992337
Lucia Salvarrey <lusalvarrey@gmai.com>
2015-11-21 21:25:35 -0300

    log out functionality

--- SAPo-FO/WebContent/js/controllers/index.sidenav.user.controller.js
+++ SAPo-FO/WebContent/js/controllers/index.sidenav.user.controller.js
@@ -3,12 +3,16 @@
     angular
         .module('sapo')
         .controller('SidenavUserController', SidenavUserController);
-    SidenavUserController.$inject = ['$scope','$cookies'];
+    SidenavUserController.$inject = ['$scope','$cookies', '$location', '$window'];
     /* @ngInject */
-    function SidenavUserController($scope,$cookies) {
+    function SidenavUserController($scope,$cookies, $location, $window) {
+    	$scope.logout = logout;
+    	$scope.redirect = redirect
+    	
         var vm = this
-    	var userTwitter = $cookies.getObject('sapoUser')
+    	var userTwitter = $cookies.getObject('sapoUser');
     	vm.userLogged = false;
+        
     	
 		if ((typeof userTwitter !== "undefined") && (userTwitter !== null)) {
 			vm.userLogged = (userTwitter.twitterId!==0);
@@ -19,6 +23,25 @@
 	        	vm.userMail = userTwitter.mail;
 	        }
 		}  
+		
+		function logout() {
+			$cookies.remove('sapoUser');
+			$cookies.remove('sapoVirtualStorages');
+			
+			//var landingUrl = $window.location.host + "/SAPo-FO/index.html";
+			
+			//$window.location.href = landingUrl;
+			
+			var landingUrl = "http://" + $window.location.host + "/SAPo-FO/index.html";
+			console.log(landingUrl);
+			$window.location.href = landingUrl;
+			
+		}
+		
+		
+        function redirect(route){
+        	$scope.$apply(function() { $location.path(route); });
+        }
         
     }
 })();
