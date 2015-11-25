4bbb3aac8d70507e9c357f5c9174422debb59abe
kavesa.laptop <kavesa@gmail.com>
2015-11-25 18:49:46 -0300

    Corrección en reports

--- SAPo-FO/WebContent/js/controllers/report.view.controller.js
+++ SAPo-FO/WebContent/js/controllers/report.view.controller.js
@@ -1,16 +1,18 @@
 (function() {
 	'use strict';
-	angular.module('sapo').controller('ReportsController',
-			ReportsController)
-			.config(function($mdDateLocaleProvider) {
-  $mdDateLocaleProvider.formatDate = function(date) {
-    return moment(date).format('YYYY-MM-DD');
-  };
-});
+	angular.module('sapo')
+	.controller('ReportsController', ReportsController)
+	.config(function($mdDateLocaleProvider) {
+		$mdDateLocaleProvider.formatDate = function(date) {
+			return moment(date).format('YYYY-MM-DD');
+		};
+	});
 	ReportsController.$inject = ['ReportsResource', '$scope' ];
 	/* @ngInject */
 	function ReportsController(ReportsResource, $scope) {
 		//$scope.title = 'Hola Araujo!!';
+//		$scope.fromDate = null;
+//		$scope.toDate = null;
 		$scope.today = new Date();
 		$scope.vsValue = '$ '+'321'+',00'; //llamar a /api/VirtualStorage/worth?id={int}
 
