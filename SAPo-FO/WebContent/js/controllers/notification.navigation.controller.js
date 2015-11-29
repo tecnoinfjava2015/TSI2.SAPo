c4622988688a46ad47e02fae2bab0d4f3458ffd9
jpmartinez <jpmc21891@gmail.com>
2015-11-29 19:15:56 -0300

    Menu
    Menu

--- SAPo-FO/WebContent/js/controllers/notification.navigation.controller.js
+++ SAPo-FO/WebContent/js/controllers/notification.navigation.controller.js
@@ -3,21 +3,23 @@
 	angular.module('sapo').controller('NotificationNavigationController',
 			NotificationNavigationController);
 	NotificationNavigationController.$inject = [ 'NotificationResource',
-			'$scope', '$location', '$cookies' ];
+			'$scope', '$location', '$cookies','$rootScope','$mdDialog' ];
 	/* @ngInject */
 	function NotificationNavigationController(NotificationResource, $scope,
-			$location, $cookies) {
-		console.log("hola1");
+			$location, $cookies,$rootScope,$mdDialog) {
+
 
 		var res = $location.path().split("/");
 		$scope.virtualStorageId = $cookies.get('sapoCurrentVirtualStorage');
 		var virtualStorages = $cookies.getObject("sapoVirtualStorages");
 		$scope.virtualStorageName = res[2];
-		$rootScope.$broadcast("menuOption","NOTIFICATION_NAVIGATION")
+		$rootScope.$broadcast("menuOption","NOTIFICATION_NAVIGATION");
+		
+		$scope.createNotification = createNotification;
 //		NotificationResource.query({
 //			tenantId : $scope.virtualStorageId
 //		}).$promise.then(function(result) {
-//			$scope.notifications = result;
+//			$scope.notifications = result; 
 //			//console.log($scope.notifications);
 //		});
 //		$scope.getNotifications = function(){
@@ -32,5 +34,24 @@
 				console.log("error");
 			});
 //		}
+
+			function createNotification(ev) {
+				$mdDialog.show({
+					controller : 'NotificationCreateController',
+					templateUrl : 'templates/notification.create.html',
+
+					parent : angular.element(document.body),
+					targetEvent : ev,
+					clickOutsideToClose : true
+				}).then(
+					function(answer) {
+						$scope.status = 'You said the information was "'
+								+ answer + '".';
+					}, function() {
+						$scope.status = 'You cancelled the dialog.';
+				});
+			
+			}			
+			
 	}
 })();
--- SAPo-FO/WebContent/js/controllers/virtualStorage.home.controller.js
+++ SAPo-FO/WebContent/js/controllers/virtualStorage.home.controller.js
@@ -1,124 +1,116 @@
 (function() {
-    'use strict';
-    angular
-        .module('sapo')
-        .controller('VirtualStorageHomeController', VirtualStorageHomeController);
-    VirtualStorageHomeController.$inject = ['$scope', '$mdDialog', '$location', '$cookies', '$rootScope',"$window"];
-    /* @ngInject */
-    function VirtualStorageHomeController($scope, $mdDialog, $location, $cookies, $rootScope,$window ) {
-    	$scope.createCategory = createCategory;
-    	$scope.editCategory = editCategory;
-    	$scope.createProduct = createProduct;
-    	$scope.createNotification = createNotification;
+	'use strict';
+	angular.module('sapo').controller('VirtualStorageHomeController',
+			VirtualStorageHomeController);
+	VirtualStorageHomeController.$inject = [ '$scope', '$mdDialog',
+			'$location', '$cookies', '$rootScope', "$window" ];
+	/* @ngInject */
+	function VirtualStorageHomeController($scope, $mdDialog, $location,
+			$cookies, $rootScope, $window) {
+		$scope.createCategory = createCategory;
+		$scope.editCategory = editCategory;
+		$scope.createProduct = createProduct;
 		$scope.redirectShoppingList = redirectShoppingList;
-	
-    	$scope.shareVS = shareVS; 
-    	var sharedVS = isShared();
+
+		$scope.shareVS = shareVS;
+		var sharedVS = isShared();
 		if (sharedVS) {
-			$rootScope.$broadcast("menuOption","VS_HOME")
-		}else{
-			$rootScope.$broadcast("menuOption","VS_HOME_CAT")
+			$rootScope.$broadcast("menuOption", "VS_HOME")
+		} else {
+			$rootScope.$broadcast("menuOption", "VS_HOME_CAT")
 		}
-		
-		function createNotification(ev) {
+
+		function createProduct(ev) {
 			$mdDialog.show({
-				controller : 'NotificationCreateController',
-				templateUrl : 'templates/notification.create.html',
+				controller : 'CreateProductController',
+				templateUrl : 'templates/product.create.html',
 
 				parent : angular.element(document.body),
 				targetEvent : ev,
 				clickOutsideToClose : true
 			}).then(
-				function(answer) {
-					$scope.status = 'You said the information was "'
-							+ answer + '".';
-				}, function() {
-					$scope.status = 'You cancelled the dialog.';
-			});
-		
+					function(answer) {
+						$scope.status = 'You said the information was "'
+								+ answer + '".';
+					}, function() {
+						$scope.status = 'You cancelled the dialog.';
+					});
+		}
+		;
+
+		function editCategory(ev) {
+			$mdDialog.show({
+				controller : 'EditCategoryController',
+				templateUrl : 'templates/category.edit.html',
+
+				parent : angular.element(document.body),
+				targetEvent : ev,
+				clickOutsideToClose : true
+			}).then(
+					function(answer) {
+						$scope.status = 'You said the information was "'
+								+ answer + '".';
+					}, function() {
+						$scope.status = 'You cancelled the dialog.';
+					});
+		}
+		;
+
+		function createCategory(ev) {
+			$mdDialog.show({
+				controller : 'CreateCategoryController',
+				templateUrl : 'templates/category.create.html',
+
+				parent : angular.element(document.body),
+				targetEvent : ev,
+				clickOutsideToClose : true
+			}).then(
+					function(answer) {
+						$scope.status = 'You said the information was "'
+								+ answer + '".';
+					}, function() {
+						$scope.status = 'You cancelled the dialog.';
+					});
+		}
+		;
+
+		function shareVS(ev) {
+			$mdDialog.show({
+				controller : 'VirtualStorageShareController',
+				templateUrl : 'templates/virtualStorage.share.html',
+				parent : angular.element(document.body),
+				targetEvent : ev,
+				clickOutsideToClose : true
+			}).then(
+					function(answer) {
+						$scope.status = 'You said the information was "'
+								+ answer + '".';
+					}, function() {
+						$scope.status = 'You cancelled the dialog.';
+					});
+		}
+		;
+
+		function redirectShoppingList() {
+			$window.location.href = "#/virtualStorage/"
+					+ $cookies.get("sapoCurrentVirtualStorageName")
+					+ "/shoppingList";
 		}
 
-		
-    	function createProduct(ev) {
-    		$mdDialog.show({
-    	    	controller: 'CreateProductController',
-                templateUrl: 'templates/product.create.html',
-    	    	
-    	        parent: angular.element(document.body),
-    	        targetEvent: ev,
-    	        clickOutsideToClose:true
-    	    })
-    	    .then(function(answer) {
-    	        $scope.status = 'You said the information was "' + answer + '".';
-    	    }, function() {
-    	        $scope.status = 'You cancelled the dialog.';
-    	    });
-    	  };
-    	
-    	function editCategory(ev) {
-      	    $mdDialog.show({
-      	    	controller: 'EditCategoryController',
-                  templateUrl: 'templates/category.edit.html',
-      	    	
-      	        parent: angular.element(document.body),
-      	        targetEvent: ev,
-      	        clickOutsideToClose:true
-      	    })
-      	    .then(function(answer) {
-      	        $scope.status = 'You said the information was "' + answer + '".';
-      	    }, function() {
-      	        $scope.status = 'You cancelled the dialog.';
-      	    });
-      	};
-      	  
-    	
-        function createCategory(ev) {
-    	    $mdDialog.show({
-    	    	controller: 'CreateCategoryController',
-                templateUrl: 'templates/category.create.html',
-    	    	
-    	        parent: angular.element(document.body),
-    	        targetEvent: ev,
-    	        clickOutsideToClose:true
-    	    })
-    	    .then(function(answer) {
-    	        $scope.status = 'You said the information was "' + answer + '".';
-    	    }, function() {
-    	        $scope.status = 'You cancelled the dialog.';
-    	    });
-    	  };
-    	  
-    	function shareVS(ev) {
-      	    $mdDialog.show({
-      	    	controller: 'VirtualStorageShareController',
-                templateUrl: 'templates/virtualStorage.share.html',
-      	        parent: angular.element(document.body),
-      	        targetEvent: ev,
-      	        clickOutsideToClose:true
-      	    })
-      	    .then(function(answer) {
-      	        $scope.status = 'You said the information was "' + answer + '".';
-      	    }, function() {
-      	        $scope.status = 'You cancelled the dialog.';
-      	    });
-      	  };
-      	
-  		function redirectShoppingList(){
-			$window.location.href = "#/virtualStorage/"+$cookies.get("sapoCurrentVirtualStorageName")+"/shoppingList";
-		}
- 
-      	function isShared() {
+		function isShared() {
 			var currentVsId = $cookies.get("sapoCurrentVirtualStorage");
 			var result = false;
 			var virtualStorages = $cookies.getObject("sapoVirtualStorages");
-			var count = virtualStorages.following.length;
+			var followingLenght = virtualStorages.following.length;
 			var i = 0;
-			for (i = 0; i < count; i++) {
-				if (currentVsId == virtualStorages.owned[i].id) {
-					result = true;
+			if (followingLenght > 0) {
+				for (i = 0; i < count; i++) {
+					if (currentVsId == virtualStorages.following[i].id) {
+						result = true;
+					}
 				}
 			}
 			return result;
 		}
-    }
+	}
 })();
